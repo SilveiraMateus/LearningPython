913bbac18a3a992b4a5458374e2bd8bda89f0afd
Mateus Silveira <mateus_silveira1@hotmail.com>
2021-12-31 09:20:34 -0300

    Primeiro Commit utilizando Python

---
+++ Python/app.py
@@ -0,0 +1,13 @@
+nome = 'Mateus'
+hobby = 'ouvir Musica'
+
+print(f'Ola, sou o {nome}  e gosto de {hobby}')
+
+b = 'ba'
+parte2 = 'nica'
+a = 'a'
+r = 'ri'
+parte1 = 'eletro'
+t = 'te'
+
+print(f'{b}{t}{r}{a} {parte1}{parte2}')
