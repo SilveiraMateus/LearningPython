47c64473a77d0a399a54aab8faf9e393d7030a09
Mateus Silveira <mateus_silveira1@hotmail.com>
2022-01-01 11:23:38 -0300

    Learned how to manage inputs and some of date conversions.

--- Python/app_new.py
+++ Python/app_new.py
@@ -1,1 +1,9 @@
-Print("Novo Arquivo")
+from datetime import datetime
+
+data_aniver = datetime.strptime(
+    input("Qual sua data de aniversario?"), '%d/%m/%Y')
+print(data_aniver)
+data_atual = datetime.now()
+dias_aniver = data_aniver - data_atual
+#tempo_em_dias = dias_aniver.days
+print(f'Faltam {dias_aniver.days} dias para o seu aniver')
